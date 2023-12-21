2310a1399e802c7433bdced1abb0ca4c2190b3d3
itc-23032 <n23032@std.it-college.ac.jp>
2023-12-21 07:42:54 +0000

    5.9 social

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,15 @@
 import Container from 'components/container'
 import Logo from 'components/logo'
+import Social from 'components/social'
 import styles from 'styles/footer.module.css'
+
 const Footer = () => {
   return (
     <footer className={styles.wrapper}>
       <Container>
         <div className={styles.flexContainer}>
           <Logo />
-          [ソーシャル]
+          <Social />
         </div>
       </Container>
     </footer>
