b3e15efcd229e30e8aa202157759cc15b18d2ecb
itc-23032 <n23032@std.it-college.ac.jp>
2024-01-12 05:38:17 +0000

    8.1

--- components/post-header.js
+++ components/post-header.js
@@ -1,17 +1,15 @@
 import styles from 'styles/post-header.module.css'
-import ConvertDate from 'components/convert-date'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faClock } from '@fortawesome/free-regular-svg-icons'
-
-const PostHeader = ({ titel, subtitle, publish = '' }) => {
+import { faClock } from '@fortawewome/free-regular-svg-icons'
+const PostHeader = ({ title, subtitle, publish = '' }) => {
   return (
     <div className={styles.stack}>
       <p className={styles.subtitle}>{subtitle}</p>
       <h1 className={styles.title}>{title}</h1>
       {publish && (
         <div className={styles.publish}>
-          <FontAwesomeIcon icon={faClock} size='la' color='var(--gray-25)' />
-          <ConvertDate dateISO={publish} />
+          <FontAwesomeIcon icon={faClock} size='lg' color='var(--gray-25)' />
+          {publish}
         </div>
       )}
     </div>
