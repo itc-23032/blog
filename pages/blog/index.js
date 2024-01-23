7de707804b7aa74dd4cba132e4de4ab2421766f8
itc-23032 <n23032@std.it-college.ac.jp>
2024-01-23 05:51:55 +0000

    9.6 画像を追加

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,9 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+
+import { eyecatchLocal } from 'lib/constants'
 
 const Blog = ({ posts }) => {
   return (
@@ -17,6 +20,14 @@ export default Blog
 
 export async function getStaticProps () {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
